22daf06281800c490694507a96bc87b14edd6f1c
Taekhyun Kim <taekhyun.kim@kau.kr>
2025-12-21 22:21:51 +0900

    updated: clock(real time) for measuring IoT model update taken time

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ async def main():
         start_time = datetime.now(timezone.utc)
         simulation_clock = SimulationClock(
             start_dt=start_time, 
-            time_step=timedelta(minutes=10),
+            time_step=timedelta(seconds=1),
             real_interval=1.0,
             sim_logger=sim_logger
         )
